dd704ce945e2911cc5a13165d5f0746483c37517
JEWESTYES <JEWESTYES@DESKTOP-SU7I2GG>
2025-03-22 16:11:37 +0300

    init

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("burger");
+    const nav = document.getElementById("nav");
 
-// Write your JavaScript code.
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        nav.classList.toggle("open");
+    });
+});
