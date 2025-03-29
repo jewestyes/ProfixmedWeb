7e37c0c4182d013b2496e978ce221d1323ed1287
JEWESTYES <JEWESTYES@DESKTOP-SU7I2GG>
2025-03-29 12:21:11 +0300

    Добавил модалку + задел на будущее + рефакторинг

---
+++ Views/ViewsComponents/EventModalViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProfixmedWeb.Models;
+
+namespace ProfixmedWeb.Views.ViewsComponents;
+
+public class EventModalViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(EventModalViewModel model)
+    {
+        return View(model);
+    }
+}
+
